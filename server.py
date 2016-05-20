bfb61c6f0550816eaad65ce91f6c61062846206f
Sameen525 <bsef14m525@pucit.edu.pk>
2016-05-20 21:23:11 +0500

    Update server.py

--- server.py
+++ server.py
@@ -6,11 +6,12 @@ import sys
 from thread import *
  
 HOST = ''   # Symbolic name meaning all available interfaces
-PORT = 8297 # Arbitrary non-privileged port
- 
+PORT = 9989 # Arbitrary non-privileged port
+ad=[]
+i=0  
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print ('Socket created')
- 
+
 #Bind socket to local host and port
 try:
     s.bind((HOST, PORT))
@@ -23,54 +24,36 @@ print 'Socket bind complete'
 #Start listening on socket
 s.listen(2)
 print 'Socket now listening'
-conn={}
-addr={}
+
+
 #Function for handling connections. This will be used to create threads
 def clientthread(conn):
     #Sending message to connected client
-    conn[0].send('Welcome to the server. Type something and hit enter\n') #send only takes string
-    conn[1].send('Welcome to the server. Type something and hit enter\n') 
-    #infinite loop so that function do not terminate and thread do not end.
+    conn.send('Welcome to the server. Type something and hit enter\n') #send only takes string
+    
     
     while True:
        
-       data = conn[1].recv(1024)
-       data1 = conn[0].recv(1024)
-       if data:
-             reply1 = "client1>>" + data
-      
-             print reply1
-             
-             conn[0].sendall(reply1)
-             
+       data = conn.recv(1024)
+       
+       print data
+       if conn== ad[0]:
+          
+          ad[1].sendall("client1>>"+data)
+       else:
+          ad[0].sendall("client2>>"+data)
              
        if not data:
-           break
-       
-       if data1:
-             reply2 = "client2>>" + data1
-             print reply2
-           
-             conn[1].sendall(reply2)
-             
+             break
+    conn.close()   
+     
 
-       if not data1:
-           break
-       
-
-    conn[0].close()
-    conn[1].close()   
- 
- 
 #now keep talking with the client
 while 1:
     #wait to accept a connection - blocking call
-    conn[0], addr[0] = s.accept()
-    print 'Connected with ' + addr[0][0] + ':' + str(addr[0][1])
-    conn[1], addr[1] = s.accept()
-    print 'Connected with ' + addr[1][0] + ':' + str(addr[1][1])
-    
-    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+    conn, addr= s.accept()
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+    ad.append(conn)
     start_new_thread(clientthread ,(conn,))
- 
+    i+=1
 s.close()
